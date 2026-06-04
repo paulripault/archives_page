e93ec8cdebb56fc241f56b0f5f39a390b6228526
Paul Ripault <paulripault@mac-1.home>
2026-06-04 19:05:02 +0200

    [FEAT] initializing springBoot

---
+++ archives/src/main/java/com/archives/archives/ArchivesApplication.java
@@ -0,0 +1,13 @@
+package com.archives.archives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivesApplication.class, args);
+	}
+
+}
