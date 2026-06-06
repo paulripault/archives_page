403cf769029440f8140016aacac39996f04a95ed
Paul Ripault <paulripault@mac-1.home>
2026-06-06 14:35:22 +0200

    [FEAT] adding first call from front to the BDD

--- archives-front/src/app/app.ts
+++ archives-front/src/app/app.ts
@@ -1,10 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
+import { ManuscriptService } from './services/manuscript.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [],
+  standalone: true,
 })
-export class App {
-  protected readonly title = signal('archives-front');
+export class App implements OnInit{
+
+  manuscripts: any[] = [];
+
+  constructor(private manuscriptService: ManuscriptService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.manuscriptService.getAll().subscribe({
+      next: data => {
+        console.log('DATA API', data);
+        this.manuscripts = data;
+        this.cdr.detectChanges()
+      },
+      error: err => {
+        console.log('API ERROR', err);
+      }
+    });
+  }
 }
---
+++ archives-front/src/app/services/manuscript.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ManuscriptService {
+    private baseUrl = "http://localhost:8080/manuscripts";
+
+    constructor(private http: HttpClient) {}
+
+    getAll(): Observable<any[]> {
+        return this.http.get<any[]>(this.baseUrl);
+    }
+
+    getById(id: number): Observable<any> {
+        return this.http.get<any>(`${this.baseUrl}/${id}`);
+    } 
+}
